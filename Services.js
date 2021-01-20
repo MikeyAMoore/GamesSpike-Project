0b450e6368fe4399a7ad92d3945eca545da837f9
Michael Andrew Moore <74065788+MikeyAMoore@users.noreply.github.com>
2021-01-20 16:47:52 -0700

    services
    added some text

---
+++ Services.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../App.css';
+
+function Services() {
+  return(
+    <div className='products'>
+      <h1>Upload</h1>
+    </div>
+  );
+}
+
+export default Services;
