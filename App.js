608664e1cf5f8753bc9ade348e60818d29e9e14a
Michael Andrew Moore <74065788+MikeyAMoore@users.noreply.github.com>
2021-01-22 19:27:00 -0700

    App JS
    Added a const

--- App.js
+++ App.js
@@ -7,9 +7,17 @@ import Services from './components/pages/Services';
 import Products from './components/pages/Products';
 import SignUp from './components/pages/SignUp';
 
+const Upload = () => (
+  <div className='container mt-4'>
+    <h4 className='display-4 text-center-4'>
+      <i className='fab fa-react' /> Games Spike File Upload
+    </h4>
+  </div>
+)
 
 function App() {
   return (
+
     
     <>
       <Router>
